feb0468e866e76c22a925125594ab70797f633f4
Lucas Oliveira <134163465+iluckr@users.noreply.github.com>
2024-08-29 19:52:36 -0300

    Update 503020.py

--- 503020.py
+++ 503020.py
@@ -3,20 +3,20 @@ user_wage = float(input("Para poder fazer a regra dos 50, 30 e 20, precisamos sa
 #50%
 a = 50 / 100
 a *= user_wage
-print("Você deve deixar R$",a,"para gastos essenciais, como, alimentação, saúde, transporte e moradia.")
+print(f"Você deve deixar R${a:.2f} para gastos essenciais, como, alimentação, saúde, transporte e moradia.")
 
 #30%
 b = 30 / 100
 b *= user_wage
-print("Você pode deixar R$",b,"para desejos pessoais, tipo, jogos, festas e etc.")
+print(f"Você pode deixar R${b:.2f} para desejos pessoais, tipo, jogos, festas e etc.")
 
 #20%
 c = 20 / 100
 c *= user_wage
-print("Você deve deixar R$",c,"para investimentos, CDB Liquidez diária, Fundos imobiliários e talvez criptomoedas.")
+print(f"Você deve deixar R${c:.2f} para investimentos, CDB Liquidez diária, Fundos imobiliários e talvez criptomoedas.")
 
-print("Gastos essenciais - 50% =",a)
-print("Desejos pessoais - 30% =",b)
-print("Investimentos - 20% =",c)
+print(f"Gastos essenciais - 50% = {a:.2f}")
+print(f"Desejos pessoais - 30% = {b:.2f}")
+print(f"Investimentos - 20% = {c:.2f}")
 
 input("")
